34860f48e14eb318d4c298222cc68f38550811d6
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-05-27 18:06:46 +0900

    Time: 23 ms (46.48%), Space: 20.5 MB (32.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,29 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        N = len(nums)
-        ans = False
-
-        q = list()
-        q.append(0)
-
-        visited = [False]*N
-        visited[0] = True
-
-        while q:
-            cur = q.pop()
-
-            if cur == N-1:
-                break
-
-            for i in range(nums[cur]):
-                if cur + i + 1 >= N:
-                    break
-                if visited[cur + i + 1] == True:
-                    continue
-                
-                q.append(cur + i + 1)
-                visited[cur + i + 1] = True
-
-        if visited[N-1] == True:
-            return True
-        return False
+        farthest = 0
+        for i in range(len(nums)):
+            if i > farthest:
+                return False
+            farthest = max(farthest, i + nums[i])
+        return True
