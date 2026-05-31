cd44415066a405cf2520c7213d4c0260ddfa1e4a
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-05-31 17:33:32 +0900

    Time: 211 ms (41.08%), Space: 24.4 MB (22.78%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.py
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -6,19 +6,24 @@ class Solution:
         # f[i]=1이면 i에 도달 가능, 0이면 i에 도달 불가능
         f, pre = [0] * n, [0] * n
         f[0] = 1
-
+        
         for i in range(minJump):
             pre[i] = 1
+        
         for i in range(minJump, n):
-            left, right = i-maxJump, i-minJump
+            left, right = i - maxJump, i-minJump
 
             if s[i] == "0":
-                total = pre[right] - (0 if left <= 0 else pre[left-1])
-                f[i] = int(total != 0)
-            pre[i] = pre[i-1] + f[i]
+                if left <= 0:
+                    total = pre[right] - 0
+                else:
+                    total = pre[right] - pre[left-1]
+                
+                if total == 0:
+                    f[i] = 0
+                else:
+                    f[i] = 1
 
+            pre[i] = pre[i - 1] + f[i]
         return bool(f[n-1])
-
-
-
 
