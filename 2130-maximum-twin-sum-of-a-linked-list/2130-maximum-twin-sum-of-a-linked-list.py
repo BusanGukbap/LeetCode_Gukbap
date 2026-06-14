18521e0b806c67ae4ba642625b531bba82a434b2
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-06-14 17:19:53 +0900

    Time: 67 ms (50.08%), Space: 50.6 MB (57.14%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,15 +5,26 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        vals = []
-        cur = head
-        while cur:
-            vals.append(cur.val)
-            cur = cur.next
+        # 1. slow/fast로 중간 지점 찾기
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        n = len(vals)
+        # 2. 뒤 절반 뒤집기  (slow부터 끝까지)
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+            
+        # 3. 앞에서부터 + 뒤에서부터 동시에 이동하며 최대합 구하기
         ans = 0
-        for i in range(n//2):
-            ans = max(ans, vals[i] + vals[n-i-1])
-        return ans
+        left, right = head, prev
+        while right:
+            ans = max(ans, left.val + right.val)
+            left = left.next
+            right = right.next
 
+        return ans
