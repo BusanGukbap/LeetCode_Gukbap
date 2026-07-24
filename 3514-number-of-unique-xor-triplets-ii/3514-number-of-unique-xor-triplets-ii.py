f8f61bfcc5a88004c741c16dc5e9aa04ca70f177
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-07-24 10:23:59 +0900

    Time: 3718 ms (77.78%), Space: 19.9 MB (11.11%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.py
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
         uniq = list(set(nums))
-        
+
+        n = len(uniq)
+
         dp = [set() for _ in range(4)]
-        dp[0].add(0)  # 0개를 선택하면 XOR은 0
-        
+        dp[0].add(0)
+
         for num in uniq:
-            for k in range(3, 0, -1):  # 역순으로 갱신 (중복 사용 방지)
-                for prev in list(dp[k - 1]):
+            for k in range(3, 0, -1):
+                for prev in list(dp[k-1]):
                     dp[k].add(prev ^ num)
         
-        # 1개 또는 3개를 선택한 결과의 합집합 크기
         return len(dp[1] | dp[3])
