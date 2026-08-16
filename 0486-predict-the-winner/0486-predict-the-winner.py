ae904486b368b1c2f8cfd882ea4eb6f50b7bf48e
DongYoon Kim <77170214+BusanGukbap@users.noreply.github.com>
2026-08-17 01:06:34 +0900

    Time: 0 ms (100%), Space: 20 MB (18.72%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -7,13 +7,10 @@ class Solution:
 
         @lru_cache(maxsize=None)
         def dfs(left: int, right: int) -> int:
-            # 현재 플레이어가 nums[left:right+1]에서 얻을 수 있는 최대 점수 차이 반환
             if left == right:
                 return nums[left]
             
-            # 왼쪽 끝을 가져가는 경우
             take_left = nums[left] - dfs(left + 1, right)
-            # 오른쪽 끝을 가져가는 경우
             take_right = nums[right] - dfs(left, right - 1)
             
             return max(take_left, take_right)
